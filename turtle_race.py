a669e4764a9e1355dda874417cc4b1ea41eaffe6
alexharrod <alexkharrod@gmail.com>
2024-08-28 18:49:14 -0400

    Turtles are all setup

--- turtle_race.py
+++ turtle_race.py
@@ -1,7 +1,26 @@
 from turtle import Turtle, Screen
 
-tim = Turtle()
+
+turtle_colors = ["blue", "red", "purple", "green", "black", 'orange']
 screen = Screen()
+screen.setup(500, 400)
+projected_winner = screen.textinput("Enter your bet.", "Which color turtle will win the race?").lower()
+print(projected_winner)
+
+
+# tim = Turtle()
+y = -60
+for num in range(6):
+    tim = Turtle("turtle")
+    tim.color(turtle_colors[num])
+    tim.penup()
+    tim.goto(x=-230,y=y)
+    y += 30
+
+
+
+
+
 
 
 screen.exitonclick()
